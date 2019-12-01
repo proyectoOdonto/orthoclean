48a0152939045066b9b93bb674496147aba694be
dannysalazarm07 <dannysalazarm07@gmail.com>
2019-12-01 18:58:24 -0500

    change structre project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,26 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+  {
     path: 'chronometers',
-    loadChildren: () => import('./chronometers/chronometers.module').then( m => m.ChronometersPageModule)
+    loadChildren: () => import('./pages/chronometers/chronometers.module').then( m => m.ChronometersPageModule)
   },
   {
     path: 'brush-info',
-    loadChildren: () => import('./brush-info/brush-info.module').then( m => m.BrushInfoPageModule)
+    loadChildren: () => import('./pages/brush-info/brush-info.module').then( m => m.BrushInfoPageModule)
   },
   {
     path: 'instruments-info',
-    loadChildren: () => import('./instruments-info/instruments-info.module').then( m => m.InstrumentsInfoPageModule)
+    loadChildren: () => import('./pages/instruments-info/instruments-info.module').then( m => m.InstrumentsInfoPageModule)
   },
   {
     path: 'exam',
-    loadChildren: () => import('./exam/exam.module').then( m => m.ExamPageModule)
+    loadChildren: () => import('./pages/exam/exam.module').then( m => m.ExamPageModule)
   },
   {
     path: 'contact-us',
-    loadChildren: () => import('./contact-us/contact-us.module').then( m => m.ContactUsPageModule)
+    loadChildren: () => import('./pages/contact-us/contact-us.module').then( m => m.ContactUsPageModule)
   },
 
 ];
