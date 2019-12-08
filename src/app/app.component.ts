b5b382311290dd05d4355f48b6f8e87a195360fd
miguel1995 <miatorresch@unal.edu.co>
2019-12-08 18:47:22 -0500

    create menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  pages: any[] = [];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router:Router
   ) {
     this.initializeApp();
   }
@@ -22,6 +27,42 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.pages = [
+        {
+          pagename: "Inicio",
+          icon: "home",
+          url:"/home"
+        },
+        {
+          pagename: "Cronometro",
+          icon: "home",
+          url:"/chronometers"
+        },
+        {
+          pagename: "Cepillado",
+          icon: "home",
+          url:"/brush-info"
+        },
+        {
+          pagename: "Instrumentos",
+          icon: "exam",
+          url:"/instruments-info"
+        },
+        {
+          pagename: "Examen",
+          icon: "home",
+          url:"/exam"
+        },
+        {
+          pagename: "Contactanos",
+          icon: "home",
+          url:"/contact-us"
+        }
+      ]
     });
+  }
+
+  goToPage(page){
+    this.router.navigate([page.url]);
   }
 }
