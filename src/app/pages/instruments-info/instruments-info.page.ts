eb132702885329f45ba4ff887826ca15ac13119f
miguel1995 <miatorresch@unal.edu.co>
2019-12-08 21:44:07 -0500

    add slides for instruments page

--- src/app/pages/instruments-info/instruments-info.page.ts
+++ src/app/pages/instruments-info/instruments-info.page.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstrumentsInfoPage implements OnInit {
 
+
+    // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+    slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+
   constructor() { }
 
   ngOnInit() {
